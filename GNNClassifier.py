444a625fc74ea3b61e271b6d98ff6f593b2f2301
omkarnaik713 <76861864+omkarnaik713@users.noreply.github.com>
2024-11-05 13:10:12 -0500

    Classifier Model Update

--- GNNClassifier.py
+++ GNNClassifier.py
@@ -16,20 +16,21 @@ class classifier(nn.Module):
             nn.Linear(input_dim, hidden_dim, bias = False),
             nn.BatchNorm1d(hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, 2*hidden_dim, bias = False),
-            nn.BatchNorm1d(2*hidden_dim)
+            nn.Linear(hidden_dim, hidden_dim//2, bias = False),
+            nn.BatchNorm1d(hidden_dim//2)
         ),train_eps=True))
         
         for i in range(5):
             self.gnn_layers.append(GINConv(nn.Sequential(
-            nn.Linear(2*hidden_dim, hidden_dim, bias = False),
+            nn.Linear(hidden_dim//2, hidden_dim, bias = False),
             nn.BatchNorm1d(hidden_dim),
             nn.ReLU(),
-            nn.Linear(hidden_dim, 2*hidden_dim, bias = False),
-            nn.BatchNorm1d(2*hidden_dim)
+            nn.Linear(hidden_dim, hidden_dim//2, bias = False),
+            nn.BatchNorm1d(hidden_dim//2)
             ),train_eps=True))
+        
         self.gnn_layers.append(GINConv(nn.Sequential(
-            nn.Linear(2*hidden_dim, hidden_dim, bias = False),
+            nn.Linear(hidden_dim//2, hidden_dim, bias = False),
             nn.BatchNorm1d(hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim, 2, bias = False)
@@ -41,15 +42,15 @@ class classifier(nn.Module):
     def forward(self, x , edge_index,batch=None ):
         for conv in self.gnn_layers:
             x = conv(x,edge_index)
-            x = F.dropout(x)
+            x = F.dropout(x, p = 0.2, training = True)
             x = self.gnn_non_linear(x)
         x = global_mean_pool(x,batch)
         return x
         
 def train(classifier,model, loader,og_data ,method, alpha):
-    opt = torch.optim.Adam(model.parameters(),lr = 0.00001, weight_decay= 1e-3)
+    opt = torch.optim.Adam(model.parameters(),lr = 0.0005, weight_decay= 5e-4)
     loss_fn = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(classifier.parameters(), lr = 0.00075, weight_decay=5e-4)
+    optimizer = torch.optim.Adam(classifier.parameters(), lr = 0.0005, weight_decay=5e-4)
     correct = 0
     total_loss = 0
     total = 0
@@ -69,9 +70,6 @@ def train(classifier,model, loader,og_data ,method, alpha):
                 total_loss += loss.item()
             print(total_loss)
         else : 
-            if i == 50 :
-                print('Classifier Training starts from here')
-                total_loss = 0
             model.eval()
             classifier.train()
             node_rep = []
@@ -81,11 +79,15 @@ def train(classifier,model, loader,og_data ,method, alpha):
             dataset = convert_df(node_rep)
             dataset['label'] = og_data.y
             print(dataset)
-            cg = discover_graph(dataset, method, alpha)
-            edge_index = edge_list(cg, method)
-            edge_index = torch.tensor(edge_index, dtype = torch.int64)
-            edge_index = edge_index.T
-            print(edge_index)
+            if i == 50 :
+                print('Classifier Training starts from here')
+                total_loss = 0
+                
+                cg = discover_graph(dataset, method, alpha)
+                edge_index = edge_list(cg, method)
+                edge_index = torch.tensor(edge_index, dtype = torch.int64)
+                edge_index = edge_index.T
+                print(edge_index)
             for batch in loader : 
                 optimizer.zero_grad()
                 
